4b79be11b50200c17be867c594d8f79587e5cae2
AlejandroCastroSanta10 <castrosantalej10@gmail.com>
2026-06-28 19:42:33 +0200

    Cambio a versión 1.0.0

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
 
     # App metadata
     app_name: str = "API de Korrijo"
-    app_version: str = "0.4.0"
+    app_version: str = "1.0.0"
 
     # CORS
     cors_origins: list[str] = ["http://localhost:3000"]
